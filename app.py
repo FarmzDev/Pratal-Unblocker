d720ef163d1447c20aa0cddc87e0f4a8553a40e7
FarmzDev <153562375+FarmzDev@users.noreply.github.com>
2024-04-17 21:41:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-clients_static_bp = Blueprint('clients_static', __name__, static_folder='clients')
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,10 +11,7 @@ def index():
 @app.route("/g")
 def games():
     return render_template("games.html")
+
 @app.route("/g/eg")
 def eaglercraft():
     return render_template("eagl.html")
-
-app.register_blueprint(clients_static_bp)
-
-app.run(debug=True, port=80)
